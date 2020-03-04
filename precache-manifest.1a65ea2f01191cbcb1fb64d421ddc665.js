75915b93c55600f37c3e706a5826140b74165acb
Paul Colwill <colwilp@mac052044.home>
2020-03-04 22:18:50 +0000

    Updates

---
+++ precache-manifest.1a65ea2f01191cbcb1fb64d421ddc665.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3230cd8060e2ab21da6d3f8c7161a7a9",
+    "url": "/southwest-log/index.html"
+  },
+  {
+    "revision": "a67c49c7a1d9fccc8c00",
+    "url": "/southwest-log/static/css/main.ae32da49.chunk.css"
+  },
+  {
+    "revision": "dd4965c0083c965b3493",
+    "url": "/southwest-log/static/js/2.86b7c2a9.chunk.js"
+  },
+  {
+    "revision": "ebe843d33fccd97c5c40b0257c9a0692",
+    "url": "/southwest-log/static/js/2.86b7c2a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a67c49c7a1d9fccc8c00",
+    "url": "/southwest-log/static/js/main.6f90c1bc.chunk.js"
+  },
+  {
+    "revision": "9deb3e9f1f6d43599004",
+    "url": "/southwest-log/static/js/runtime-main.64554860.js"
+  },
+  {
+    "revision": "1ead2443819e41994ee7eff888d34e47",
+    "url": "/southwest-log/static/media/map-full.1ead2443.png"
+  },
+  {
+    "revision": "b142127c3a2170b8d34ddad46986d87b",
+    "url": "/southwest-log/static/media/southwestlogs-bg.b142127c.jpeg"
+  }
+]);
